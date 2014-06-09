8a8e7aaeda1e96bd857abf06c8671781be762b64
tuhuayuan <tuhuayuan@gmail.com>
2014-06-09 21:38:58 +0800

    list page update

--- tracer/handlers.py
+++ tracer/handlers.py
@@ -5,6 +5,7 @@ import tornado.web
 import tornado.gen
 import qrcode
 import os
+import math
 from models import Tracer
 from StringIO import StringIO
 from tornado.httpclient import AsyncHTTPClient
@@ -77,7 +78,7 @@ class Login(BaseHandler):
 
 class TracerManager(BaseHandler):
     """tracer管理
-    @method: edit|add|list|remove
+    @method: update|add|list|remove
     @key:
     """
     @tornado.web.authenticated
@@ -85,15 +86,24 @@ class TracerManager(BaseHandler):
         if method == 'add':
             self.get_add_tracer()
         elif method == 'list':
-            self.get_list_tracer(key)
+            self.get_list_tracer(int(key))
+        elif method == 'update':
+            self.get_update_tracer(str(key))
+        else:
+            self.send_error(404)
+
 
     @tornado.web.authenticated
     @tornado.gen.coroutine
     def post(self, method, key):
         if method == 'add':
             yield self.post_add_tracer()
+        elif method == 'remove':
+            yield self.post_remove_tracer(str(key))
+        elif method == 'update':
+            yield self.post_update_tracer(str(key))
 
-    @tornado.gen.coroutine
+
     def get_add_tracer(self):
         obj = Tracer(
             id=Tracer.gen_id(12),
@@ -128,8 +138,48 @@ class TracerManager(BaseHandler):
                 self.reverse_url('qr_viewer', new_id) + "?perisist=1&api=" + QRViewer.__api_key__)
         self.redirect(self.reverse_url('tracer_manager', 'list', 0))
 
-    def get_list_tracer(self, page=0):
-        self.render("tracer_list.html")
+    def get_list_tracer(self, page=0, per=10):
+        q = self.dbsession.query(Tracer)
+        tracers = q.order_by(Tracer.posted.desc()).offset(page * per).limit(per).all()
+        if len(tracers) == 0 and page != 0:
+            self.redirect(self.reverse_url('tracer_manager', 'list', 0))
+        counter = self.dbsession.query(Tracer.id)
+        page_count = int(math.ceil(float(counter.count()) / per))
+        if page > page_count - 1 or page < 0:
+            page = 0
+        self.render("tracer_list.html",
+                tracers = tracers,
+                page=page,
+                page_count=page_count)
+
+    def get_update_tracer(self, tracer_id):
+        tracer = self.dbsession.query(Tracer).get(tracer_id)
+        if tracer is None:
+            raise tornado.web.HTTPError(404)
+        self.render("tracer_update.html", tracer=tracer)
+
+
+    @tornado.gen.coroutine
+    def post_update_tracer(self, tracer_id):
+        tracer = self.dbsession.query(Tracer).get(tracer_id)
+        if tracer is None:
+            raise tornado.web.HTTPError(404)
+        tracer.title = self.get_body_argument('tracer_title', default='')
+        tracer.content = self.get_body_argument('tracer_content', default='')
+        tracer.status = self.get_body_argument('tracer_status', default=0)
+        self.dbsession.commit()
+        self.redirect(self.reverse_url('tracer_manager', 'list', 0))
+
+
+    @tornado.gen.coroutine
+    def post_remove_tracer(self, tracer_id):
+        t = self.dbsession.query(Tracer).get(tracer_id)
+        if t is None:
+            raise tornado.web.HTTPError(404)
+        self.dbsession.delete(t)
+        self.dbsession.commit()
+        page = self.get_body_argument("page", default=0)
+        self.redirect(self.reverse_url('tracer_manager', 'list', page))
 
 class TracerShower(BaseHandler):
     """
@@ -142,16 +192,26 @@ class QRViewer(BaseHandler):
     """生成指定键值的二维码图片
     @api: 直接生成图片，返回json结果
     @perisist: 保存图片
+    @static: 直接返回静态图片，不存在则返回404
     """
     __api_key__ = "43vxl8jazpNJ"
+    __qr_static__ = "qr"
+    __kind__ = 'png'
 
     def get(self, key):
         perisist = bool(self.get_query_argument("perisist", default=''))
         api = self.get_query_argument("api", default='')
+        static = self.get_query_argument("static", default='')
         url = 'http://' + self.request.host + self.reverse_url('tracer', key)
-        path = os.path.join(self.settings['static_path'], 'qr')
+        path = os.path.join(self.settings["static_path"], QRViewer.__qr_static__)
         if not os.path.exists(path):
             os.makedirs(path)
+
+        # 直接返回静态图片
+        if bool(static):
+           self.redirect(self.static_url('qr/%s.%s' % (key, QRViewer.__kind__)),
+                   permanent=True)
+           return
 
         # 固定一个二维码大小和密度以适应打印到包装上
         qr = qrcode.QRCode(
@@ -166,11 +226,11 @@ class QRViewer(BaseHandler):
 
         # 如果是持久化则首先检查文件是否存在
         self.set_header("Content-Type", "image/%s" % img.kind)
-        if perisist and \
+        if bool(perisist) and \
                 (self.get_current_user() is not None or api == QRViewer.__api_key__):
-            path = os.path.join(path, '%s.%s' % (key, img.kind))
+            path = os.path.join(path, '%s.%s' % (key, QRViewer.__kind__))
             if not os.path.exists(path):
-                img.save(path, img.kind)
+                img.save(path, QRViewer.__kind__)
             if bool(api):
                 self.write(dict(result='OK'))
             else:
